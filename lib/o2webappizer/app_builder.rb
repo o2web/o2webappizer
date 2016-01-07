18aeb1f2d12455f02e9b49c39d5c3cadbdbf7981
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-06 21:14:17 -0500

    builder configured and readme + init_git tasks added

--- lib/o2webappizer/app_builder.rb
+++ lib/o2webappizer/app_builder.rb
@@ -1,7 +1,11 @@
 module O2webappizer
   class AppBuilder < Rails::AppBuilder
     def readme
-      template 'README.md.erb', 'README.md'
+      template 'README.md.erb', 'README.md', force: true
+    end
+
+    def init_git
+      git :init
     end
   end
 end
--- lib/o2webappizer/generators/app_generator.rb
+++ lib/o2webappizer/generators/app_generator.rb
@@ -9,13 +9,29 @@ module O2webappizer
     class_option :skip_bundle, type: :boolean, aliases: "-B", default: true,
       desc: "Don't run bundle install"
 
+    class_option :skip_test_unit, type: :boolean, aliases: "-T", default: true,
+      desc: "Skip Test::Unit files"
+
     def finish_template
       invoke :o2webappizer_customization
       super
     end
 
     def o2webappizer_customization
+      invoke :setup_git
+    end
 
+    def setup_git
+      if !options[:skip_git]
+        say 'Initializing git'
+        build :init_git
+      end
+    end
+
+    protected
+
+    def get_builder_class
+      O2webappizer::AppBuilder
     end
   end
 end
