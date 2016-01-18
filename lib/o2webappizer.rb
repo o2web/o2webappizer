1d599bd5f04a74169ee72888cea01bdd86bda2c2
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-18 16:13:49 -0500

    require 'active_support/all'

--- lib/o2webappizer.rb
+++ lib/o2webappizer.rb
@@ -1,7 +1,7 @@
 require "o2webappizer/version"
 require "o2webappizer/generators/app_generator"
 require "o2webappizer/app_builder"
-require 'active_support/core_ext/string/indent'
+require 'active_support/all'
 
 module O2webappizer
 end
